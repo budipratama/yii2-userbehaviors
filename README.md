dc7cf62ab47542db1c57ac0c239a126bdd63bdcc
budi pratama <budipratama@icode.co.id>
2022-08-25 14:56:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+User Behaviors
+==============
+User Behaviors
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist budipratama/yii2-userbehaviors "*"
+```
+
+or add
+
+```
+"budipratama/yii2-userbehaviors": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \budipratama\behaviors\AutoloadExample::widget(); ?>```
---
+++ UserBehaviors.php
@@ -0,0 +1,76 @@
+<?php
+namespace budipratama\behaviors;
+
+use yii\base\InvalidCallException;
+use yii\behaviors\AttributeBehavior;
+use yii\db\BaseActiveRecord;
+echo "bud";die();
+/**
+ * This is just an example.
+ */
+class UserBehaviors extends AttributeBehavior
+{
+    /**
+     * @var string the attribute that will receive Yii::$app->user->id value
+     * Set this property to false if you do not want to record the creation time.
+     */
+    public $createdAtAttribute = 'first_user';
+    /**
+     * @var string the attribute that will receive Yii::$app->user->id value.
+     * Set this property to false if you do not want to record the update time.
+     */
+    public $updatedAtAttribute = 'last_user';
+    /**
+     * {@inheritdoc}
+     *
+     * In case, when the value is `null`, the result of the value Yii::$app->user->id
+     * will be used as value.
+     */
+    public $value;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => [$this->createdAtAttribute, $this->updatedAtAttribute],
+                BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->updatedAtAttribute,
+            ];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * In case, when the [[value]] is `null`, the result of the value Yii::$app->user->id
+     * will be used as value.
+     */
+    protected function getValue($event)
+    {
+        if ($this->value === null) {
+            return 1;//Yii::$app->user->id;
+        }
+
+        return parent::getValue($event);
+    }
+
+    /**
+     * Updates a Yii::$app->user->id attribute to the current user id.
+     * @param string $attribute the name of the attribute to update.
+     * @throws InvalidCallException if owner is a new record (since version 2.0.6).
+     */
+    public function touch($attribute)
+    {
+        /* @var $owner BaseActiveRecord */
+        $owner = $this->owner;
+        if ($owner->getIsNewRecord()) {
+            throw new InvalidCallException('Updating the Yii::$app->user->id is not possible on a new record.');
+        }
+        $owner->updateAttributes(array_fill_keys((array) $attribute, $this->getValue(null)));
+    }
+}
